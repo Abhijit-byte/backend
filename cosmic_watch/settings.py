8fcbecf0d3a2f7dcac13fecfd59df7cb296f41da
Abhijit Dash <atd5370@gmail.com>
2026-02-08 11:40:58 +0530

    Add 'comet-sgis.vercel.app' to ALLOWED_HOSTS

--- cosmic_watch/settings.py
+++ cosmic_watch/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cosmic-watch-dev-key'
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,comet-sgis.vercel.app
+').split(',')
 
 INSTALLED_APPS = [
     'daphne',
